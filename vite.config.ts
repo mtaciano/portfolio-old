6df4863e44be62aec95c5c9c6e42124096b05778
Miguel Taciano <contact@mtaciano.com>
2023-09-16 18:45:28 -0300

    primeiro commit

---
+++ vite.config.ts
@@ -0,0 +1,11 @@
+import solid from "solid-start/vite";
+import { defineConfig } from "vite";
+import netlify from "solid-start-netlify";
+
+export default defineConfig({
+  plugins: [
+    solid({
+      adapter: netlify({ edge: true }),
+    }),
+  ],
+});
