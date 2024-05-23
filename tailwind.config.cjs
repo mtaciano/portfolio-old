99ab752942d6ffcbeda61f6b12d95995accb1a62
Miguel Taciano <contact@mtaciano.com>
2024-05-22 21:45:59 -0300

    change default font and change font type

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,7 +7,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ["Poppins", ...defaultTheme.fontFamily.sans],
+        sans: ["Rubik", ...defaultTheme.fontFamily.sans],
         serif: ["DM Serif Display", ...defaultTheme.fontFamily.serif],
         mono: ["Roboto Mono", ...defaultTheme.fontFamily.mono],
       },
